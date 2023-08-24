3d67fa744308b1ec5c0d14634e02e99805d08f9c
amesp73 <amitrpatel502@gmail.com>
2023-08-23 20:46:49 -0500

    class 3 exercises

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,9 @@
+package exercises.classespart1.src.main.java;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String topic;
+    private Teacher instructor;
+    private ArrayList<Student> enrolledStudents;
+}
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,18 @@
+package exercises.classespart1.src.main.java;
+
 public class StudentPractice {
-    public static void main(String[] args){
-        //insantiate your Student class below
+    public static void main(String[] args) {
+        //instantiate your Student class below
+        Student newStudent = new Student("Ami", 12345, 1, 4.0);
+        System.out.println(newStudent.getStudentId());
+
+        /*
+         CAN ALSO DO
+        Student newStudent = new Student();
+        newStudent.setName("Ami");
+        newStudent.setStudentId(12345)
+        ...
+        */
+
     }
 }
