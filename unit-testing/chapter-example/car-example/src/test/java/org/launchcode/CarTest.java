3ec8f653cb4d289b8d9384f0427f8eea98474f4d
amesp73 <amitrpatel502@gmail.com>
2023-09-10 16:07:31 -0500

    CarTest.java completed first 2 tests successfully

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -1,10 +1,17 @@
 package org.launchcode;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
+    Car test_car;
+    @BeforeEach
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
+
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
     @Test
     public void emptyTest() {
@@ -13,7 +20,7 @@ class CarTest {
     //TODO: constructor sets gasTankLevel properly
     @Test
     public void testInitialGasLevel() {
-        Car test_car = new Car("Toyota", "Prius",  10, 50);
+//        Car test_car = new Car("Toyota", "Prius",  10, 50);
         assertEquals(10, test_car.getGasTankLevel(), 001);
     }
     //TODO: gasTankLevel is accurate after driving within tank range
