24853343e2f7ce908ab2431b1b934a825829af30
amesp73 <amitrpatel502@gmail.com>
2023-09-24 15:09:06 -0500

    completed exercises for debugging

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,15 @@
 package org.launchcode;
 
+
 import java.util.HashMap;
+
+
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+
+        Divide(6, 0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -12,16 +17,41 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+
         // Test out your CheckFileExtension() function!
+        int studentPoints = 0;
+
+        try {
+            studentPoints = CheckFileExtension("");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            studentPoints -= 1;
+        }
+        System.out.println(studentPoints);
+        System.out.println(CheckFileExtension(studentFiles.get("Brad")));
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static void Divide(int x, int y) {
+
+        try {
+            System.out.println(x / y);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+
+        if (fileName == null || fileName.isEmpty()) {
+            throw new IllegalArgumentException("Student did not submit a file!");
+        }
+
+        if(fileName.endsWith(".java")) {
+            return 1;
+        } else {
+            return 0;
+        }
+
     }
 }
